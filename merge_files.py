04b47a6dfc7acf0b3df7ca8d0921392c8db3072c
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2023-04-10 15:23:08 +0300

    fixing duplications

--- merge_files.py
+++ merge_files.py
@@ -2,6 +2,7 @@ import io
 import json
 
 def merge_json_files(files):
+    dupliactes = 0
     # Initialize empty sets for the verses and references
     all_verses = set()
     all_references = set()
@@ -10,11 +11,28 @@ def merge_json_files(files):
     for file in files:
         with open(file, 'r', encoding='utf-8') as f:
             data = json.load(f)
-        all_verses.update(data['verses'])
-        all_references.update(data['references'])
+        len_before = len(all_verses)
+        verses_data = data['verses']
+        refs_data = data['references']
+        for i in range(len(verses_data)-1, -1, -1):
+            if i == 0:
+                print()
+            all_verses.add(verses_data[i])
+            len_now = len(all_verses)
+            if len_before != len_now:
+                len_before = len(all_references)
+                all_references.add(refs_data[i])
+                len_now = len(all_references)
+                if len_before == len_now:
+                    all_verses.remove(verses_data[i])
+                    dupliactes += 1
+            else:
+                dupliactes += 1
 
     # Combine the verses and references into a single dictionary
     combined_data = {'verses': list(set(all_verses)), 'references': list(set(all_references))}
+
+    print(f"There were {dupliactes} duplicated verses.")
 
     # Create combined file
     with io.open(f'merged_data.json', 'w', encoding='utf8') as outfile:
