cce075bbe3c327119a287090979b80efef21bbe9
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2023-06-05 14:01:36 +0300

    fix bug

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import merge_files
 # Limit the length of every scraped verse (verses with more chars than CHARS_LIMIT will not be scraped)
 # Set to -1 to disable limit
 CHARS_LIMIT = 175
-TRANSLATION = "ESV"
+TRANSLATION = "KJV"
 
 # tags = ['patience']
 # tags = ['faith', 'strength', 'healing', 'trust', 'love', 'heal', 'motivation', 'hope']
-tags = ['joy', 'happiness']
+tags = ['love']
 # tags = ['foster_care', 'adoption', 'children', 'family_forgiveness', 'caring', 'forgiveness', 'parenting', 'family', 'love', 'goodness', 'helping']
 # tags = ['motivation', 'love', 'inspiration', 'joy', 'peace', 'kindness', 'goodness', 'faith', 'spiritual_growth', 'courage']
 # tags = ['motivation', 'love', 'inspiration', 'joy', 'peace', 'kindness', 'goodness', 'faith', 'spiritual_growth', 'courage', 'patience']
@@ -17,6 +17,7 @@ tags = ['joy', 'happiness']
 if __name__ == "__main__":
     count_verses = 0;
     json_files = list()
+    merged_file: str = None
     for tag in tags:
         output = extractor.extract(tag, CHARS_LIMIT)
         json_files.append(output[0])
@@ -32,6 +33,10 @@ if __name__ == "__main__":
 
     if (TRANSLATION != "ESV"):
         print(f"\nTranslating to {TRANSLATION}...")
-        extractor.extract_other_translation(merged_file, TRANSLATION)
+        if merged_file is not None:
+            extractor.extract_other_translation(merged_file, TRANSLATION)
+        else:
+            extractor.extract_other_translation(json_files[0], TRANSLATION)
+
 
     print(f"\nDONE Scraping {count_verses} verses in {TRANSLATION}!!!")
